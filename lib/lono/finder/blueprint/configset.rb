7f0b0a00d2919acb5e359638b115693354bf2de0
Tung Nguyen <tongueroo@gmail.com>
2019-12-24 19:48:03 +0000

    finder blueprint configset spec

--- lib/lono/finder/blueprint/configset.rb
+++ lib/lono/finder/blueprint/configset.rb
@@ -1,4 +1,33 @@
 class Lono::Finder::Blueprint
-  class Configset < Base
+  class Configset < Lono::Finder::Configset
+    def initialize(options={})
+      super
+      @blueprint_root = options[:blueprint_root] || Lono.blueprint_root
+    end
+
+    # Special method for Downloader. Does not consider materialized configsets
+    def find_local(name)
+      found = local.find { |i| i["name"] == name }
+      return found["path"] if found
+    end
+
+    def local
+      blueprint + vendor
+    end
+
+    def all
+      blueprint + vendor + materialized
+    end
+
+    def blueprint
+      roots = path_roots("#{@blueprint_root}/app/#{type.pluralize}")
+      components(roots, "blueprint-local")
+    end
+
+    # Folders that each materialized gems to tmp/configsets
+    def materialized
+      roots = path_roots("#{@lono_root}/tmp/#{type.pluralize}")
+      components(roots, "materialized-local")
+    end
   end
 end
