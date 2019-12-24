90971256e52eb71f24b01a020c6337805723fe30
Tung Nguyen <tongueroo@gmail.com>
2019-12-24 19:49:55 +0000

    spec find_local

--- spec/lono/finder/blueprint/configset_spec.rb
+++ spec/lono/finder/blueprint/configset_spec.rb
@@ -3,7 +3,7 @@ describe Lono::Finder::Blueprint::Configset do
   let(:lono_root) { nil }
   let(:blueprint_root) { nil }
 
-  context "project local" do
+  context "blueprint local" do
     let(:blueprint_root) { "spec/fixtures/finder/blueprint-configset/blueprint_only" }
     it "find" do
       root_path = configset.find("ssm")
@@ -11,12 +11,22 @@ describe Lono::Finder::Blueprint::Configset do
       root_path = configset.find("non-existing")
       expect(root_path).to be nil
     end
+
+    it "find_local" do
+      root_path = configset.find_local("ssm")
+      expect(root_path).to include "blueprint_only/app/configsets/ssm"
+    end
   end
 
   context "vendor local" do
     let(:lono_root) { "spec/fixtures/finder/blueprint-configset/vendor_only" }
     it "find" do
       root_path = configset.find("ssm")
+      expect(root_path).to include "vendor_only/vendor/configsets/ssm"
+    end
+
+    it "find_local" do
+      root_path = configset.find_local("ssm")
       expect(root_path).to include "vendor_only/vendor/configsets/ssm"
     end
   end
@@ -29,12 +39,19 @@ describe Lono::Finder::Blueprint::Configset do
       root_path = configset.find("ssm")
       expect(root_path).to include "materialized_only/tmp/configsets/ssm"
     end
+
+    it "find_local" do
+      allow(configset).to receive(:gem_roots).and_return(["spec/fixtures/finder/blueprint-configset/materialized_only"])
+
+      root_path = configset.find_local("ssm")
+      expect(root_path).to be nil
+    end
   end
 
   context "both blueprint and vendor" do
     let(:blueprint_root) { "spec/fixtures/finder/blueprint-configset/both" }
     let(:lono_root) { "spec/fixtures/finder/blueprint-configset/both" }
-    it "find higher precedence project local" do
+    it "find higher precedence blueprint local" do
       root_path = configset.find("ssm")
       expect(root_path).to include "both/app/configsets/ssm"
     end
