27670bdf63e6fa4a0c2ce9fd3cff28cd82da1223
Tung Nguyen <tongueroo@gmail.com>
2019-12-28 22:43:04 +0000

    update cli docs

--- lib/lono/blueprint.rb
+++ lib/lono/blueprint.rb
@@ -7,13 +7,13 @@ module Lono
     register(New, "new", "new NAME", "Generates new lono blueprint.")
 
     desc "list", "Lists project blueprints"
-    long_desc Help.text("blueprint/new")
+    long_desc Help.text("blueprint/list")
     def list
       Finder::Blueprint.list
     end
 
     desc "configsets", "List blueprints configsets"
-    long_desc Help.text("blueprint/new")
+    long_desc Help.text("blueprint/configsets")
     option :stack, desc: "stack name. defaults to blueprint name."
     option :verbose
     def configsets(blueprint)
