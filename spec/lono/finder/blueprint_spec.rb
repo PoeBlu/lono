ec5e6104c146bd6441baea524081d208a3af5f89
Tung Nguyen <tongueroo@gmail.com>
2019-12-29 02:13:50 +0000

    fix more specs

--- spec/lono/finder/blueprint_spec.rb
+++ spec/lono/finder/blueprint_spec.rb
@@ -4,7 +4,7 @@ describe Lono::Finder::Blueprint do
   context "project local" do
     let(:lono_root) { "spec/fixtures/finder/blueprint/project_only" }
     it "find" do
-      root_path = blueprint.find("ec2")
+      root_path = blueprint.find("ec2").root
       expect(root_path).to include "project_only/app/blueprints/ec2"
       root_path = blueprint.find("non-existing")
       expect(root_path).to be nil
@@ -14,7 +14,7 @@ describe Lono::Finder::Blueprint do
   context "vendor local" do
     let(:lono_root) { "spec/fixtures/finder/blueprint/vendor_only" }
     it "find" do
-      root_path = blueprint.find("ec2")
+      root_path = blueprint.find("ec2").root
       expect(root_path).to include "vendor_only/vendor/blueprints/ec2"
     end
   end
@@ -24,7 +24,7 @@ describe Lono::Finder::Blueprint do
     it "find" do
       allow(blueprint).to receive(:gem_roots).and_return(["spec/fixtures/finder/blueprint/gem_only"])
 
-      root_path = blueprint.find("ec2")
+      root_path = blueprint.find("ec2").root
       expect(root_path).to eq "spec/fixtures/finder/blueprint/gem_only"
     end
   end
@@ -32,7 +32,7 @@ describe Lono::Finder::Blueprint do
   context "both project and vendor" do
     let(:lono_root) { "spec/fixtures/finder/blueprint/both" }
     it "find higher precedence project local" do
-      root_path = blueprint.find("ec2")
+      root_path = blueprint.find("ec2").root
       expect(root_path).to include "both/app/blueprints/ec2"
     end
   end
